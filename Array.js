7bb8fc1b3eec72c8385ed5d6047ee1cdbb41baaa
Igor <jigorua@gmail.com>
2022-09-24 23:17:23 +0300

    sample code

---
+++ Array.js
@@ -0,0 +1,29 @@
+const shopping = ['bread', 'milk', 'cheese', 'hummus', 'noodles']; // Масив
+const addLastElement = shopping.push("cat"); // Додавання елементу в кінці масиву
+const addFirstElement = shopping.unshift("cat"); // Додавання елементу в початок масиву
+
+// ВИДАЛЕННЯ ЕЛЕМЕНТІВ З МАСИВУ 
+
+const deleteLastElement = shopping.pop(); // Видалення елементу в кінці масиву
+const deleteFirstElement = shopping.shift(); // Видалення елементу на початку масиву
+
+// ОБ'ЄДНАННЯ МАСИВІВ
+
+const whatBying = shopping.concat(shoppingList);
+const whatByingToo = shopping.concat(shoppingList, foods); // Об'єднання кількох масивів
+
+// Знаходження індексу елементу в масиві
+
+const index = shopping.indexOf("milk");
+
+// ПЕРЕТВОРЕННЯ МАСИВА У РЯДОК
+
+const flowers = ["rose", "lily", "daisies"];
+const arrayToString = flowers.join();
+"rose,lily,daisies"; // Результат
+const arrayToStringSecond = flowers.join( - );
+"rose - lily - daisies"; // Результат
+
+// ГЕНЕРАТОР ВИПАДКОВИХ ЧИСЕЛ ТА СЛІВ
+
+var randomBodyPart
