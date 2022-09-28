799b89e540e5015a80d08175ef99d70364340b32
Igor <jigorua@gmail.com>
2022-09-28 21:50:20 +0300

    Додавання значення, знаходження значення, об'єднання масивів та об'єктві

--- Objects.js
+++ Objects.js
@@ -3,10 +3,44 @@
 let cat = {
     "legs": 3,
     "name": "jsut cat",
-    "age": "3 month"
+    age: "3 month" // МОЖНА БЕЗ ЛАПОК, АЛЕ ТОДІ НЕМОЖНА СТАВИТИ ПРОБІЛ
 };
 
 // ДОБУВАННЯ ЗНАЧЕННЯ З ОБ'ЄКТА
 
 cat["name"]; // ЯКЩО Є ПРОБІЛИ В КЛЮЧІ
 cat.name; // ЯКЩО НЕМАЄ ПРОБІЛІВ В КЛЮЧІ
+Object.keys(cat); // ВИДОБУТТЯ УСІХ КЛЮЧІВ
+
+// ДОДАВАННЯ ЗНАЧЕННЯ ДО ОБ'ЄКТА
+
+cat["legs"] = 3;
+cat["name"] = "just cat"; // ДОДАВАННЯ ЗНАЧЕННЯ
+
+cat.legs = 3;
+cat.name = "just cat"; // ДОДАВАННЯ ЗНАЧЕННЯ ЧЕРЕЗ КРАПКУ
+
+Object {legs: 3, name: "just cat", age: "3 month"}; // КЛЮЧІ НЕ Є ПОСЛІДОВНИМИ В ОБ'ЄКТІ ЯК В МАСИВІ
+
+// ПОЄДНАННЯ МАСИВУ ТА ОБ'ЄКТА
+
+var cars = [
+    { name: "BMW", color: "black"},
+    { name: "Lamba", color: "purple"},
+    { name: "Lexus", color: "white"}
+];
+
+cars[0];
+{ name: "BMW", color: "black"}; // ЗНАХОДЖЕННЯ ЗНАЧЕННЯ ПЕРШОГО ОБ'ЄКТУ
+cars[0]["name"]; // ЗНАХОДЖЕННЯ ПЕРШОГО ЗНАЧЕННЯ
+cars[0].name; // ЗНАХОДЖЕННЯ ЗНАЧЕННЯ ЗА ДОПОМОГОЮ КРАПКИ
+
+// МАСИВ ДРУЗІВ
+
+let bohdan = { name: "Bohdan", age: 23, passwords: [321, "goodMoon"] };
+let christina = { name: "Christina", age: 12, passwords: [21, "iLoveYou"] };
+let daryna = { name: "Daryna", age: 34, passwords: ["solovei", 843] };
+
+let friends = [bohdan, christina, daryna]; // СТВОРЮЄМО МАСИВ З ДРУЗІВ
+
+friends[1].passwords[1]
