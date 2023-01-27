042f6961b93ea474f10fdd1fe0ed8bc8c4f385c0
Dimex <jigorua@gmail.com>
2023-01-27 13:39:26 +0200

    learn if and else

--- loops.js
+++ loops.js
@@ -3,3 +3,12 @@
 If (condition) { // if твердження перевіряє, чи умова є правильною - True
     console.log("Do something"); // КОД, ЩО ЗАПУСКАЄТЬСЯ, ЯКЩО УМОВА БУДЕ TRUE, ЗВЕТЬСЯ ТІЛОМ
 }
+
+var name = "Igor"; // If...else твердження
+
+console.log("Hello " + name);
+if (name.length >= 4) {
+    console.log("Wow, you have a REALLY short name!");
+} else {
+    console.log("Your name is very long");
+}
